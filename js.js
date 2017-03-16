c00459e4725605c4070bfa5445884e1879b95f7e
Patrick <pbentley07@gmail.com>
2017-03-16 19:56:09 -0400

    First codebase

---
+++ js.js
@@ -0,0 +1,7 @@
+$(function(){
+       $("#typed").typed({
+           strings: ["Come <span id='Bold'>Live</span> In Savannah", "Come Work In Savannah", " Come Grow In Savannah"],
+           typeSpeed: 20,
+           loop:true
+       });
+   });
