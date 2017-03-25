12bfd538912a18eac6016aba1ed45f821cc7222a
Patrick <pbentley07@gmail.com>
2017-03-25 10:18:33 -0400

    Small styling changes and quote change

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
 $(function(){
        $("#typed").typed({
-           strings: ["Come <span id='bold'>Live</span> In Savannah.", "Come <span id='bold'>Work</span> In Savannah.", " Come <span id='bold'>Grow</span> In Savannah."],
+           strings: ["Come <span id='bold'>Live</span> In Savannah.^1000", "Come <span id='bold'>Work</span> In Savannah.^1000", " Come <span id='bold'>Grow</span> In Savannah.^2000"],
            typeSpeed: 20,
-           cursorChar: "|*"
+           cursorChar: "|*",
+           loop:true
+
        });
    });
